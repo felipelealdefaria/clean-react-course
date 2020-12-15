37f2d842d59818ea9918c080493778c552e89467
felipelealdefaria <felipelealdefaria@gmail.com>
2020-12-15 00:32:34 -0300

    feat: ensure RemoteAuthentication thwrows InvalidCrendetialsError on 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { HttpResponse } from './http-response'
+
 export type HttpPostParams = {
   url: string
   body?: object
 }
 
 export interface HttpPostClient {
-  post (params: HttpPostParams): Promise<void>
+  post (params: HttpPostParams): Promise<HttpResponse>
 }
