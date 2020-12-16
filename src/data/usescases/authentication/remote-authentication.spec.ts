0229141c04c3073b15d6817fd71939f6ee78d806
felipelealdefaria <felipelealdefaria@gmail.com>
2020-12-16 11:18:47 -0300

    fix: adjust HttpStatusCode typo

--- src/data/usescases/authentication/remote-authentication.spec.ts
+++ src/data/usescases/authentication/remote-authentication.spec.ts
@@ -43,7 +43,7 @@ describe('RemoteAuthentication', () => {
   test('Should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.unathorized
+      statusCode: HttpStatusCode.unauthorized
     }
     const promise = sut.auth(mockAuthentication())
 
