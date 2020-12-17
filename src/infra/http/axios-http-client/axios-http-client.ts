a080eea5c038fb9f32e1ea3f26d450fcda672db6
felipelealdefaria <felipelealdefaria@gmail.com>
2020-12-17 11:11:26 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from 'axios'
+import { HttpPostParams } from '@/data/protocols/http'
+
+export class AxiosHttpClient {
+  async post (params: HttpPostParams<any>): Promise<any> {
+    await axios(params.url)
+  }
+}
